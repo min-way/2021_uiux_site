1331d72d59dc175e326a9ef28ba02fc65b8b7216
min-way <nissyou@naver.com>
2021-05-04 12:23:27 +0900

    -

--- 05company/js/script.js
+++ 05company/js/script.js
@@ -2,7 +2,15 @@ $(document).ready(function(){
   $(".slider").bxSlider({
     pager:false
   });
-$(".conversion ul li").click(function(){
-  $(this).addClass("on").siblings().removeClass("on");
-});
+
+  $(".conversion ul li").click(function(){
+    $(this).addClass("on").siblings().removeClass("on");
+  });
+
+  // $(window).resize(function(){
+  //   console.log(window.innerWidth)
+  //   if(window.innerWidth <= 600){
+  //
+  //   }
+  // });
 });
