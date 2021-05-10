131cb2ee41e0ced5d2c42c66e79bf044127ee88a
min-way <nissyou@naver.com>
2021-05-10 23:44:22 +0900

    -

--- 05company/js/script.js
+++ 05company/js/script.js
@@ -10,31 +10,37 @@ $(document).ready(function(){
   const topBtn = document.getElementsByClassName("topBtn")[0];
   topBtn.onclick = function(){
     // window.scrollTo(0,0);
-    $("html,body").animate({scrollTop:0},1000)
+    $("html,body").animate({scrollTop:0},700)
   }
 
-  var swiper = new Swiper(".mySwiper", {
-        slidesPerView: 1,
-        spaceBetween: 30,
-        loop: true,
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-      });
+    var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 1,
+      spaceBetween: 5,
+      loop: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
 
 
+  // var swiper = new Swiper('.swiper-container', {
+  //   slidesPerView: 1,
+  //   spaceBetween: 3,
+  //   breakpoints: {
+  //     700 : {
+  //       slidesPerView: 2,
+  //       spaceBetween: 20
+  //     }
+  // })
 
-  $(window).resize(function(){
+  // $(window).resize(function(){
+  //   if(window.innerWidth <= 700){
 
-    if(window.innerWidth <= 700){
-      $(".contMoBox img").attr("src","images/bg_Business.png");
-    }else{
-      $(".contMoBox img").attr("src","images/icon03.png");
-    }
-  });
+  //   }
+  // });
 });
