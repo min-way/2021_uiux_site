af97978f0f1e17dcc17ca18a3c83df0c2e576333
min-way <nissyou@naver.com>
2021-05-10 23:44:45 +0900

    -

--- 05company/js/script.js
+++ 05company/js/script.js
@@ -13,30 +13,6 @@ $(document).ready(function(){
     $("html,body").animate({scrollTop:0},700)
   }
 
-    var swiper = new Swiper(".mySwiper", {
-      slidesPerView: 1,
-      spaceBetween: 5,
-      loop: true,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-    });
-
-
-  // var swiper = new Swiper('.swiper-container', {
-  //   slidesPerView: 1,
-  //   spaceBetween: 3,
-  //   breakpoints: {
-  //     700 : {
-  //       slidesPerView: 2,
-  //       spaceBetween: 20
-  //     }
-  // })
 
   // $(window).resize(function(){
   //   if(window.innerWidth <= 700){
